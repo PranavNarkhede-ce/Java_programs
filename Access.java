d3ff62040c1ad9363840d6b7e236bc7bd5fa84a3
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-16 19:29:32 +0530

    Add program demonstrating access specifiers in java

---
+++ Access.java
@@ -0,0 +1,37 @@
+class Demo
+{
+    public int i;
+    private int j;
+    protected int k;
+
+    public Demo()
+    {
+        i = 0;
+        j = 0;
+        k = 0;
+    }
+
+    public void Display()
+    {
+        System.out.println("value of i:" + i);              //allowed
+        System.out.println("value of j:" + j);              //allowed
+        System.out.println("value of k:" + k);              //allowed
+
+
+    }
+}
+
+class Access 
+{
+    public static void main(String A[])
+    {
+        Demo dobj = new Demo();
+
+        dobj.Display();
+
+        System.out.println("value of i:" + dobj.i);             //allowed
+        System.out.println("value of j:" + dobj.j);             //not allowed
+        System.out.println("value of k:" + dobj.k);             //  ?
+
+    }
+}
