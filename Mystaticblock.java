808e58a4f27bde86b8a13a3c3c4c9e7e8895a6c7
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-18 17:38:07 +0530

    Add program demonstrating static block execution

---
+++ Mystaticblock.java
@@ -0,0 +1,12 @@
+class Mystaticblock
+{
+    static
+    {
+        System.out.println("inside static block");
+    }
+
+    public static void main(String []A)
+    {
+        System.out.println("inside main  method ");
+    }
+}
