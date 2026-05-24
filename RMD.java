effa2f123f059249754cca39a93452eee2d42539
pranavnarkhede45-ce <pranavnarkhede45@gmail.com>
2026-05-24 20:10:08 +0530

    Add program demonstrating runtime method dispatch

---
+++ RMD.java
@@ -0,0 +1,42 @@
+class Base
+{
+    public int i , j ;
+
+    public void fun()
+    {  System.out.println("Inside Base fun");   }
+
+    public void gun()
+    {  System.out.println("Inside Base gun");   }
+
+    public void sun()
+    {  System.out.println("Inside Base sun");   }
+
+}
+
+class Derived extends Base
+{
+    public int x , y ;
+
+    public void gun()
+    {  System.out.println("Inside Derived gun");  }
+
+    public void sun()
+    {  System.out.println("Inside Derived sun");  }
+
+    public void run()
+    {  System.out.println("Inside Derived run");  }
+
+}
+
+class RMD
+{
+    public static void main(String A[])
+    {
+        // Base bobj = new Base();             // no casting
+        Base bobj = new Derived();              // upcasting
+        // Derived dobj = new Derived();           // no casting
+        Derived dobj = new Base();                  // downcasting 
+        
+
+    }
+}
